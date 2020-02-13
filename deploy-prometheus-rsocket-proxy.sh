a39df52b110243308c829efaf7697af84becc3b3
Toshiaki Maki <tmaki@pivotal.io>
2020-02-14 02:07:29 +0900

    update

--- deploy-prometheus-rsocket-proxy.sh
+++ deploy-prometheus-rsocket-proxy.sh
@@ -3,6 +3,7 @@
 kapp deploy -a rsocket-proxy --diff-changes \
   -f <(ytt -f prometheus-rsocket-proxy/scripts/kubernetes/proxy/ \
            -f ops-files/small-foot-print.yml \
+           -f ops-files/metrics-tags.yml \
            -f ops-files/gateway.yml \
-           $@)
+           $@ | kbld -f -)
 
