43bdfdd7bc0d9abe2aed32662dd07e4c9bd20050
Toshiaki Maki <tmaki@pivotal.io>
2020-05-02 15:17:47 +0900

    Fix tag

--- deploy-prometheus-rsocket-proxy.sh
+++ deploy-prometheus-rsocket-proxy.sh
@@ -3,6 +3,7 @@
 kapp deploy -a rsocket-proxy --diff-changes \
   -f <(ytt -f vendor/prometheus-rsocket-proxy/scripts/kubernetes/proxy/ \
            -f overlays/small-foot-print.yml \
+           -f overlays/fix-tag.yml \
            -f overlays/metrics-tags.yml \
            -f overlays/gateway.yml \
            $@ | kbld -f -)
