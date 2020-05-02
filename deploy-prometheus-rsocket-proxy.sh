d76ea7c29691bbafb629016fac36b4a585e7eeb0
Toshiaki Maki <tmaki@pivotal.io>
2020-05-02 15:24:30 +0900

    add healtcheck

--- deploy-prometheus-rsocket-proxy.sh
+++ deploy-prometheus-rsocket-proxy.sh
@@ -4,6 +4,7 @@ kapp deploy -a rsocket-proxy --diff-changes \
   -f <(ytt -f vendor/prometheus-rsocket-proxy/scripts/kubernetes/proxy/ \
            -f overlays/small-foot-print.yml \
            -f overlays/fix-tag.yml \
+           -f overlays/healthcheck.yml \
            -f overlays/metrics-tags.yml \
            -f overlays/gateway.yml \
            $@ | kbld -f -)
