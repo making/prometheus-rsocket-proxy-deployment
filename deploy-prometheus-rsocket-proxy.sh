dbdcb2a1babd2350aaa239c3002aaa12df7adefa
Toshiaki Maki <tmaki@pivotal.io>
2020-05-02 14:45:50 +0900

    Use vendor instead of git submodule

--- deploy-prometheus-rsocket-proxy.sh
+++ deploy-prometheus-rsocket-proxy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 kapp deploy -a rsocket-proxy --diff-changes \
-  -f <(ytt -f prometheus-rsocket-proxy/scripts/kubernetes/proxy/ \
+  -f <(ytt -f vendor/prometheus-rsocket-proxy/scripts/kubernetes/proxy/ \
            -f ops-files/small-foot-print.yml \
            -f ops-files/metrics-tags.yml \
            -f ops-files/gateway.yml \
